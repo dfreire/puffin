1dc3593563f25d29e625742cdbb96137220c75c4
dfreire <dario.freire@gmail.com>
2016-01-09 00:31:46 +0000

    update

--- backend/jsx/main.jsx
+++ backend/jsx/main.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Navbar from "./navbar.jsx";
+
+const Root = React.createClass({
+    render() {
+        return (
+            <Navbar lang="en" />
+        );
+    }
+});
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Root />,
   document.getElementById("root")
 );
---
+++ backend/jsx/navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default React.createClass({
+    getInitialState() {
+        return { items: [] };
+    },
+    componentDidMount() {
+        $.getJSON("/navbar.json", (items) => {
+            this.setState({items: items});
+        });
+    },
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">
+                            <img alt="Brand" src="/puffin.png" />
+                        </a>
+                    </div>
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav">
+                            {this._renderItems()}
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    },
+    _renderItems() {
+        return this.state.items.map((item) => {
+            return (
+                <li><a href={item.path}>{item.title[this.props.lang]}</a></li>
+            );
+        });
+
+    }
+});
