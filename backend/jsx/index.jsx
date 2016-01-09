8c48381a7a47eb65ac02c5e4c1c219cce5292ae6
dfreire <dario.freire@gmail.com>
2016-01-09 19:46:15 +0000

    update

--- backend/jsx/index.jsx
+++ backend/jsx/index.jsx
@@ -16,7 +16,6 @@ const Navbar = React.createClass({
                 <div className="container">
                     <div className="navbar-header">
                         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span className="sr-only">Toggle navigation</span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
