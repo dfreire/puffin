c3d31a7d340c347e5915ed2db3d41ee0a3bfcaec
dfreire <dario.freire@gmail.com>
2016-01-10 15:20:54 +0000

    update

--- backend/jsx/components/list.jsx
+++ backend/jsx/components/list.jsx
@@ -7,9 +7,57 @@ export default React.createClass({
     },
     render() {
         return (
-            <table className="table table-striped table-hover table-bordered">
-                {this._renderHead()}
-            </table>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">{this._renderFilter()}</div>
+                    <div className="col-md-8">{this._renderTable()}</div>
+                </div>
+            </div>
+        );
+    },
+    _renderFilter() {
+        return (
+            <div className="panel panel-default">
+                {this._renderHeading()}
+                <div className="panel-body">
+                    {this.props.blueprint.fields.map((field, i) => {
+                        let label = field.label[this.props.activeLanguage];
+                        return (
+                            <div key={i} className="form-group">
+                                <input type="text" className="form-control" placeholder={label} />
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    },
+    _renderHeading() {
+        return (
+            <div className="panel-heading">
+                <div className="pull-left"><b>Users</b></div>
+                <div className="pull-right">
+                    <a href="#">create</a><span className="text-muted">&nbsp;|&nbsp;</span>
+                    <a href="#">update</a><span className="text-muted">&nbsp;|&nbsp;</span>
+                    <a href="#">remove</a><span className="text-muted">&nbsp;|&nbsp;</span>
+                    <a href="#">clear</a>
+                </div>
+                <div className="clearfix"></div>
+            </div>
+        );
+    },
+    _renderTable() {
+        return (
+            <div className="panel panel-default">
+                {this._renderHeading()}
+                <div className="panel-body">
+                    <table className="table table-striped table-hover">
+                        {this._renderHead()}
+                        {this._renderBody()}
+                    </table>
+                </div>
+                <div className="panel-footer">{this._renderPagination()}</div>
+            </div>
         );
     },
     _renderHead() {
@@ -17,12 +65,48 @@ export default React.createClass({
             return (
                 <thead>
                     <tr>
+                        <th style={{width: 40}}><input type="checkbox" /></th>
                         {this.props.blueprint.fields.map((field, i) => {
-                            return <td key={i}>{field.label[this.props.activeLanguage]}</td>
+                            return <th key={i}>{field.label[this.props.activeLanguage]}</th>
                         })}
                     </tr>
                 </thead>
             );
         }
+    },
+    _renderBody() {
+        return (
+            <tbody>
+                <tr>
+                    <td><input type="checkbox" /></td>
+                    {this.props.blueprint.fields.map((field, i) => {
+                        return <td key={i}>{field.label[this.props.activeLanguage]}</td>
+                    })}
+                </tr>
+            </tbody>
+        );
+    },
+    _renderPagination() {
+        return (
+            <nav style={{textAlign: "center"}}>
+                <ul className="pagination" style={{margin: 0}}>
+                    <li>
+                        <a href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li>
+                        <a href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        );
     }
 });
